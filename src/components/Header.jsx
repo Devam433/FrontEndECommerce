26c8aa6911a04e33a9d1fe189ea1e4d4f47cc9af
Devam <devam433@gmail.com>
2024-08-17 19:26:13 +0530

    add hammenu

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,23 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { navItems } from '../constants/index.js';
 import Button from './Button.jsx';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCartShopping, faSearch, faUser } from '@fortawesome/free-solid-svg-icons';
+import { faBars, faCartShopping, faSearch, faTimes, faUser } from '@fortawesome/free-solid-svg-icons';
 import { useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 
 function Header() {
-
+  const [isMenuOpen,setIsMenuOpen] = useState(false);
   const cartItems = useSelector(state=>state.cart.cart);
   const navigate = useNavigate();
+
+  function handleMenu(){
+    setIsMenuOpen(prev=>!prev);
+  }
+function handleHamMenu(url){
+  setIsMenuOpen(false);
+  navigate(url);
+}
   return (
     <header className='w-full h-[55px] bg-white flex justify-center fixed top-0 left-0 z-50 shadow-md'>
       <div className='flex w-[97%] h-full bg-white items-center justify-between sm:justify-center gap-3'>
-        <div className='sm:hidden w-[35px] h-[35px] bg-slate-800 rounded-full'></div>{/* ham menu */}
-        
+        <div className='sm:hidden flex justify-center items-center w-[50px] h-[45px] bg-slate-800 rounded-[50px]' onClick={handleMenu}>{/* ham menu */}
+            <FontAwesomeIcon icon={isMenuOpen ? faTimes: faBars } size='lg' color='white'/>
+            
+        </div>
         <section className='hidden sm:flex  w-[40%] sm:w-[30%] md:w-[19%] gap-3 h-full items-center'>
           <div className='flex items-center'>{/* logo */}
-            <p className='text-4xl font-extrabold text-heading-bold-black'>SHOP.CO</p>
+            <p className='text-4xl font-extrabold text-heading-bold-black' onClick={()=>navigate('/')}>SHOP.CO</p>
           </div>
         </section>
         
@@ -57,6 +67,20 @@ function Header() {
           </div>
         </section>
       </div>
+      {
+            isMenuOpen ?
+            <menu className=' absolute top-[50px] border border-gray-400 bg-[#929397f7] rounded-lg flex justify-center w-[95%] px-[30px]'>
+                <nav className='flex flex-col items-center gap-2 text-lg text-navItem p-2 w-full'>
+                {
+                  navItems.map(item => (
+                    <Button key={item.id} className='px-2 py-1 text-white text-lg w-full hover:bg-gray-300 transition-colors hover:text-black' onClick={()=>handleHamMenu(item.url)}>{item.value}</Button>
+                  ))
+                }
+                </nav>
+            </menu>
+            :
+            ''
+            }
     </header>
   );
 }
